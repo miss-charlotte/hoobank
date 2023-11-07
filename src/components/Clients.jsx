4d02a1f77bf0721e157621440be9480406eec735
miss-jagger <charlotte.akongo@gmail.com>
2023-11-07 11:23:29 +0300

    first commit

---
+++ src/components/Clients.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { clients } from '../constants'
+import styles from '../style'
+
+const Clients = () => {
+  return (
+    <section className={`${styles.flexCenter} my-4`}>
+      <div className={`${styles.flexCenter} flex-wrap w-full`}>
+        {clients.map((client) => (
+          <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:w-[192px] min-w-[120px]`}>
+            <img src={client.logo} alt='client' className='sm:w-[162px] w-[75px] object-contain hover:text-secondary cursor-pointer'/>
+          </div>
+        ))}
+      </div>
+    </section>
+  )
+}
+
+export default Clients
